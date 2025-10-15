d5268cf0cd1ada4287e1016da54f157039dd3a88
Stephan <foxelyss@gmail.com>
2025-10-15 11:13:29 +0700

    Includes

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="py-3 my-4">
+	<ul class="nav justify-content-center border-bottom pb-3 mb-3">
+		<li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><?= $footer_1 ?></a></li>
+		<li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><?= $footer_2 ?></a></li>
+		<li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><?= $footer_3 ?></a></li>
+		<li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><?= $footer_4 ?></a></li>
+	</ul>
+	<p class="text-center text-muted">© 2025 Stepashkin Company, Inc</p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#">php.zone</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">Foxelyss</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><?= $header_1 ?></a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link" href="#"><?= $header_2 ?></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><?= $header_3 ?></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><?= $header_4 ?></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
